fabe683dcd9e6e24040ef404a82983734f5661ca
tonysh518 <tony@fuel-it-up.com>
2013-02-02 12:19:17 +0800

    html version

---
+++ js/strictly.js
@@ -0,0 +1,12 @@
+ $.backstretch("images/bg.jpg");
+ 
+ $('.who_slider').cycle({ 
+	fx:     'fade',  
+	timeout: 4000, 
+	speed: 500,
+	width: 615,
+	height: 408,
+	fit: 1,
+	cleartypeNoBg: true,
+	sync: true
+});
